798ce8257b4bfd693cfaac101552e3432f83cf55
AronKinoue <30123@ojs.ac.jp>
2020-06-08 14:52:23 +0900

    配列の初期化と中身の処理

--- SampleSC/CodeFile1.cs
+++ SampleSC/CodeFile1.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Windows.Forms;
 
 class CodeFile1 : Form
@@ -11,6 +12,11 @@ class CodeFile1 : Form
     //デフォルトコンストラクタ
     public CodeFile1()
     {
+        var scores = new int[] { 54, 77, 12, 53, 48 };
 
+        for (int i = 0; i < scores.Length; i++)
+        {
+            Console.WriteLine(scores[i]);
+        }
     }
 }
