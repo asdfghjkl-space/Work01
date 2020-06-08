0aebeba3ab0e3cac4783e3acc4f570bb459c379c
AronKinoue <30123@ojs.ac.jp>
2020-06-08 13:59:07 +0900

    新規登録

---
+++ SampleSC/CodeFile1.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Windows.Forms;
+
+class CodeFile1
+{
+    static Form form;
+
+    static void Main()
+    {
+        form = new Form();
+        form.Width = 800;
+        form.Height = 600;
+
+        Application.Run(form);
+
+    }
+}
