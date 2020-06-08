14c9b4ed554c80971b658a43c8e2fa8172b79b5b
AronKinoue <30123@ojs.ac.jp>
2020-06-08 13:23:17 +0900

    プロジェクト ファイルを追加します。

---
+++ Sample0602/FormApp.cs
@@ -0,0 +1,114 @@
+﻿using System.Reflection;
+using System.Windows.Forms;
+using System.Drawing;
+
+class FormApp
+{
+    static Form form;
+    static Button buttonAppName;
+    public static void Main()
+    {
+        Timer timer = new Timer();  //タイマー
+
+        timer.Tick += Timer_Tick;
+        timer.Interval = 5;
+        timer.Start();
+
+        form = new Form();  //フォームの生成
+
+        form.Text = "フォームアプリケーション";
+        form.Width = 800;  //幅
+        form.Height = 600;  //高さ
+        form.BackColor = Color.Red;
+
+        //ラベル
+        Label labelAppName = new Label();  //ラベル生成
+        labelAppName.Text = "サンプルアプリ";
+        labelAppName.Parent = form;  //ラベルの親を設定
+
+        //ボタン
+        buttonAppName = new Button();  //ボタンを生成
+        buttonAppName.Text = "ボタン";
+        buttonAppName.Top = 30; //上から30（ｙ座標）
+        buttonAppName.Left = 50; //左から50（ｘ座標）
+        buttonAppName.Width = 50;
+        buttonAppName.Height = 50;
+        buttonAppName.Click += ButtonAppName_Click;
+        buttonAppName.Parent = form;  //ラベルの親を設定
+
+
+        Application.Run(form);  //フォームを指定して実行
+    }
+
+    private static void Timer_Tick(object sender, System.EventArgs e)
+    {
+        #region//switch文で色の切り替え
+        switch (a)
+        {
+            case 0:
+                {
+                    form.BackColor = Color.White;
+                    a++;
+                    break;
+                }
+
+            case 1:
+                {
+                    form.BackColor = Color.Black;
+                    a--;
+                    break;
+                }
+        }
+        #endregion
+
+    }
+
+    static int a = 0;
+    private static void ButtonAppName_Click(object sender, System.EventArgs e)
+    {
+        //form.BackColor = Color.White;
+
+        //if文で色の切り替え
+        //if(form.BackColor == Color.Red)
+        //{
+        //    form.BackColor = Color.Blue;
+        //}
+        //else
+        //{
+        //    form.BackColor = Color.Yellow;
+        //}
+
+        #region//switch文で色の切り替え
+        switch (a)
+        {
+            case 0:
+                {
+                    form.BackColor = Color.White;
+                    a++;
+                    break;
+                }
+
+            case 1:
+                {
+                    form.BackColor = Color.Black;
+                    a--;
+                    break;
+                }
+        }
+        #endregion
+
+        #region//ボタンの色切り替え
+        Button bt = (Button)sender;
+        if(bt.BackColor == Color.Gray)
+        {
+            bt.BackColor = Color.Black;
+        }
+        else
+        {
+            bt.BackColor = Color.Gray;
+        }
+        #endregion
+
+        //MessageBox.Show("ボタンが押されたよ");
+    }
+}
---
+++ Sample0604/CodeFile1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class CodeFile
+{
+    static Form form;
+    static Label labelRed;
+    static Label labelBlue;
+    static Label labelYellow;
+    static Button button;
+    
+    public static void Main()
+    {
+        form = new Form();
+
+        form.Text = "hahahaha";
+        form.Width = 800;
+        form.Height = 600;
+        form.BackColor = Color.White;
+
+        labelRed = new Label();
+
+        labelRed.Text = "";
+        labelRed.Parent = form;
+        labelRed.Width = 100;
+        labelRed.Height = 100;
+        labelRed.Top = 300;
+        labelRed.Left = 300;
+        //labelRed.BackColor = Color.White;
+        labelRed.BackColor = Color.Transparent;
+        labelRed.BorderStyle = BorderStyle.FixedSingle;
+
+
+        labelBlue = new Label();
+
+        labelBlue.Text = "";
+        labelBlue.Parent = form;
+        labelBlue.Width = 100;
+        labelBlue.Height = 100;
+        labelBlue.Top = 300;
+        labelBlue.Left = 100;
+        //labelBlue.BackColor = Color.Black;
+        labelBlue.BackColor = Color.Transparent;
+        labelBlue.BorderStyle = BorderStyle.FixedSingle;
+        
+
+
+        labelYellow = new Label();
+
+        labelYellow.Text = "";
+        labelYellow.Parent = form;
+        labelYellow.Width = 100;
+        labelYellow.Height = 100;
+        labelYellow.Top = 300;
+        labelYellow.Left = 200;
+        labelYellow.BackColor = Color.Transparent;
+        labelYellow.BorderStyle = BorderStyle.FixedSingle;
+
+
+        button = new Button();
+
+        button.Text = "色チェンジ";
+        button.Top = 30;
+        button.Left = 50;
+        button.Width = 100;
+        button.Height = 50;
+        button.Click += Button_Click;
+        button.Parent = form;
+
+        Application.Run(form);
+    }
+
+    //static int a = 0;
+
+    enum COLOR_ID
+    {
+        COLOR_ID_RED,
+        COLOR_ID_YELLOW,
+        COLOR_ID_BLUE,
+    }
+    static COLOR_ID color_id = COLOR_ID.COLOR_ID_BLUE;
+    
+    
+    private static void Button_Click(object sender, EventArgs e)
+    {
+        labelYellow.BackColor = Color.Transparent;
+        labelBlue.BackColor = Color.Transparent;
+        labelRed.BackColor = Color.Transparent;
+
+        //if(labelRed.BackColor == Color.Red)
+        //{
+        //    labelRed.BackColor = Color.Red;
+        //    labelBlue.BackColor = Color.Black;
+        //    labelYellow.BackColor = Color.Black;
+        //}
+        //else if(labelYellow.BackColor == Color.Black)
+        //{
+        //    labelRed.BackColor = Color.Black;
+        //    labelYellow.BackColor = Color.Yellow;
+        //    labelBlue.BackColor = Color.Black;
+        //}
+        //else
+        //{
+        //    labelRed.BackColor = Color.Red;
+        //}
+
+
+
+        switch (color_id)
+        {
+            case COLOR_ID.COLOR_ID_BLUE:
+                {
+                    labelBlue.BackColor = Color.Blue;
+                    //labelYellow.BackColor = Color.Black;
+                    //labelRed.BackColor = Color.Black;
+                    //a++;
+                    color_id = COLOR_ID.COLOR_ID_YELLOW;
+                    break;
+                }
+            case COLOR_ID.COLOR_ID_YELLOW:
+                {
+                    //labelBlue.BackColor = Color.Black;
+                    labelYellow.BackColor = Color.Yellow;
+                    //labelRed.BackColor = Color.Black;
+                    //a++;
+                    color_id = COLOR_ID.COLOR_ID_RED;
+                    break;
+                }
+            case COLOR_ID.COLOR_ID_RED:
+                {
+                    //labelBlue.BackColor = Color.Black;
+                    //labelYellow.BackColor = Color.Black;
+                    labelRed.BackColor = Color.Red;
+                    color_id = COLOR_ID.COLOR_ID_BLUE;
+                    break;
+                }
+
+        }
+    }
+}
