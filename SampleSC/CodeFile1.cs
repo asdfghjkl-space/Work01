5531c0e8bb3c1fb6cd9971c0fe796a8b1eb2040e
AronKinoue <30123@ojs.ac.jp>
2020-06-09 15:54:13 +0900

    演習６

--- SampleSC/CodeFile1.cs
+++ SampleSC/CodeFile1.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Linq;
 using System.Windows.Forms;
 
 class CodeFile1 : Form
@@ -73,30 +74,65 @@ class CodeFile1 : Form
         //}
         #endregion
 
-
         #region//*を三角に表示
-        int num;
+        //int num;
 
-        Console.Write("段数 : ");
-        num = int.Parse(Console.ReadLine());
+        //Console.Write("段数 : ");
+        //num = int.Parse(Console.ReadLine());
 
-        Console.WriteLine();//改行
+        //Console.WriteLine();//改行
 
-        for (int j = 0; j < num; j++)
-        {
-            //空白を入れる
-            for (int i = 0; i < (num - j) - 1; i++)
-            {
-                Console.Write(" ");
-            }
+        //for (int j = 0; j < num; j++)
+        //{
+        //    //空白を入れる
+        //    for (int i = 0; i < (num - j) - 1; i++)
+        //    {
+        //        Console.Write(" ");
+        //    }
 
-            //アスタリスク出力
-            for (int i = 0; i < (j + 1); i++)
-            {
-                Console.Write(" *");
-            }
-            Console.WriteLine();//改行
-        }
+        //    //アスタリスク出力
+        //    for (int i = 0; i < (j + 1); i++)
+        //    {
+        //        Console.Write(" *");
+        //    }
+        //    Console.WriteLine();//改行
+        //}
+        #endregion
+
+
+        #region//Stringの勉強
+        //stringの別名(エイリアス)がString
+        //string moji = "";
+
+        //moji = Console.ReadLine();
+
+        ////Tolow大文字入力を小文字に変換
+        ////ToUpp小文字入力を大文字に変換
+        //Console.WriteLine("入力された文字 : " + moji.ToUpper());
+        #endregion
+
+        #region//演習5 入力された文字をカウント
+        //string moji = "";
+
+        //moji = Console.ReadLine();
+
+        //Console.WriteLine("文字列 : " + moji.ToLower());
+        //Console.WriteLine("文字数 : " + moji.Count());
+        #endregion
+
+        #region//文字の置換
+        //string moji = "";
+        //string moji2 = "";
+        //string moji3 = "";
+
+        //moji = Console.ReadLine();
+        //moji2 = Console.ReadLine();
+        //moji3 = Console.ReadLine();
+
+        //Console.WriteLine("文字列 : " + moji.ToLower());
+        //Console.WriteLine("置換前 : " + moji2.ToLower());
+        //Console.WriteLine("置換後 : " + moji3.ToLower());
+        //Console.WriteLine("文字列 : " + moji.Replace(moji2, moji3));
         #endregion
     }
 }
