6a595f08df71368ffbbe6420f52e5cdd58aa3fbd
AronKinoue <30123@ojs.ac.jp>
2020-06-09 13:46:25 +0900

    アスタリスク出力

--- SampleSC/CodeFile1.cs
+++ SampleSC/CodeFile1.cs
@@ -12,28 +12,52 @@ class CodeFile1 : Form
     //デフォルトコンストラクタ
     public CodeFile1()
     {
+        //var scores = new int[5];
+
+        ////値の入力
+        //for (var i = 0; i < scores.Length; i++)
+        //{
+        //    //整数値の取り込み
+        //    var num = Console.ReadLine();
+
+        //    var number = int.Parse(num);
+
+        //    scores[i] = number;
+        //}
+        ////合計値
+        //var total = 0;
+
+        ////値の出力
+        //for (int i = 0; i < scores.Length; i++)
+        //{
+        //    total += scores[i];
+        //}
+
+        //Console.WriteLine("入力された値の合計は、"+total+"です。");
+
+
+
+        //アスタリスク出力
         var scores = new int[5];
 
-        //値の入力
-        for (var i = 0; i < scores.Length; i++)
-        {
-            //整数値の取り込み
-            var num = Console.ReadLine();
-
-            var number = int.Parse(num);
-
-            scores[i] = number;
-        }
-        //合計値
-        var total = 0;
-
-        //値の出力
+        //入力
         for (int i = 0; i < scores.Length; i++)
         {
-            total += scores[i];
+            Console.Write("scores[" + i + "]=");
+            scores[i] = int.Parse(Console.ReadLine());
         }
 
-        Console.WriteLine("入力された値の合計は、"+total+"です。");
+        Console.WriteLine();//改行
+
+        //出力
+        for (int j = 0; j < scores.Length; j++)
+        {
+            for (int i = 0; i < scores[j]; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();//改行
+        }
 
         //for(int i = 0; i< scores.Length; i++)
         //{
