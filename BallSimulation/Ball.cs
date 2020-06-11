222c5d1dc8b0ee504c7dde2c5492f9f368d72701
AronKinoue <30123@ojs.ac.jp>
2020-06-11 17:56:50 +0900

    ennsyuu

--- BallSimulation/Ball.cs
+++ BallSimulation/Ball.cs
@@ -8,6 +8,7 @@ class Ball
     public double YPos { get; set; }   //X座標
     public Image  Img  { get; set; }   //画像データ
 
+    
     private double moveX;   //加速度(X)
     private double moveY;   //加速度(Y)
 
@@ -22,9 +23,22 @@ class Ball
         moveY = 5;
     }
 
+    //引数付きコンストラクタ(座標指定)
+    public Ball(double xp,double yp)
+    {
+        XPos = xp;
+        YPos = yp;
+        Img = Image.FromFile(@"images\soccer_ball.png");
+
+        moveX = 5;
+        moveY = 5;
+    }
+
     public void Move()
     {
         XPos += moveX;
         YPos += moveY;
     }
+
+
 }
--- BallSimulation/CodeFile1.cs
+++ BallSimulation/CodeFile1.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using System.Security.Cryptography.X509Certificates;
 using System.Windows.Forms;
 
 class Stage : Form
@@ -21,22 +22,41 @@ class Stage : Form
         Height = 800;
         Text = "ボールシミュレーション";
 
-        ball = new Ball();   //ボールオブジェクトの生成
+        //ball = new Ball();   //ボールオブジェクトの生成
         PictureBox pictureBox = new PictureBox();
 
-        pictureBoxBall.Width = 100;
-        pictureBoxBall.Height = 100;
-        pictureBoxBall.Top = (int)ball.XPos;
-        pictureBoxBall.Left = (int)ball.YPos;
-        pictureBoxBall.Image = ball.Img;
-        pictureBoxBall.SizeMode = PictureBoxSizeMode.StretchImage;
+        //pictureBoxBall.Width = 100;
+        //pictureBoxBall.Height = 100;
+        //pictureBoxBall.Top = (int)ball.XPos;
+        //pictureBoxBall.Left = (int)ball.YPos;
+        //pictureBoxBall.Image = ball.Img;
+        //pictureBoxBall.SizeMode = PictureBoxSizeMode.StretchImage;
         pictureBoxBall.Parent = this;
 
-        timer.Interval = 10;        //ms
+        //timer.Interval = 10;        //ms
+        //timer.Tick += Timer_Tick;   //時間経過で呼ぶメソッドを登録
+        //timer.Start();              //タイマースタート
+
+        this.MouseClick += Stage_MouseClick;
+    }
+
+    //マウスクリック時のイベントハンドラ
+    private void Stage_MouseClick(object sender, MouseEventArgs e)
+    {
+        ball = new Ball(e.X-50,e.Y-50);   //ボールオブジェクトの生成
+
+        //クリックした座標を使いボールオブジェクトを生成
+        pictureBoxBall.Width = 100;
+        pictureBoxBall.Height = 100;
+        pictureBoxBall.Top = (int)ball.YPos;
+        pictureBoxBall.Left = (int)ball.XPos;
+        pictureBoxBall.Image = ball.Img;
+        pictureBoxBall.SizeMode = PictureBoxSizeMode.StretchImage;
+        //pictureBoxBall.Parent = this;
+
+        timer.Interval = 5000;      //ms
         timer.Tick += Timer_Tick;   //時間経過で呼ぶメソッドを登録
         timer.Start();              //タイマースタート
-
-
     }
 
     //指定した時間が経過すると呼ばれるメソッド
