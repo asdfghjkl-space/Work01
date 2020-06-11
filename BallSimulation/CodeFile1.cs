50f7be03c0c37dc5143991ad9cc3e85ec7376595
AronKinoue <30123@ojs.ac.jp>
2020-06-11 13:42:55 +0900

    ステージ追加

---
+++ BallSimulation/CodeFile1.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.Windows.Forms;
+
+class Stage : Form
+{
+    public static void Main()
+    {
+        Application.Run(new Stage());
+    }
+
+    public Stage()
+    {
+        //フォームの設定
+        Width = 800;
+        Height = 600;
+        Text = "ボールシミュレーション";
+
+    }
+}
