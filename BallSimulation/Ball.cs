8301c538c1a5ab58eb25271036fadc9f0fa4ada9
AronKinoue <30123@ojs.ac.jp>
2020-06-11 14:23:35 +0900

    ボールクラスの作成と画像の追加

---
+++ BallSimulation/Ball.cs
@@ -0,0 +1,18 @@
+﻿
+//ボールクラス
+using System.Drawing;
+
+class Ball
+{
+    public double XPos { get; set; }   //X座標
+    public double YPos { get; set; }   //X座標
+    public Image  Img  { get; set; }   //画像データ
+
+    //コンストラクタ
+    public Ball()
+    {
+        XPos = 0;
+        YPos = 0;
+        Img = Image.FromFile(@"images\soccer_ball.png");
+    }
+}
