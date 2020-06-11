c63344d7d234799a1442644b70d8662d2c65ba5d
AronKinoue <30123@ojs.ac.jp>
2020-06-11 16:00:46 +0900

    演習のつづき

--- BallSimulation/Ball.cs
+++ BallSimulation/Ball.cs
@@ -8,11 +8,23 @@ class Ball
     public double YPos { get; set; }   //X座標
     public Image  Img  { get; set; }   //画像データ
 
+    private double moveX;   //加速度(X)
+    private double moveY;   //加速度(Y)
+
     //コンストラクタ
     public Ball()
     {
         XPos = 0;
         YPos = 0;
         Img = Image.FromFile(@"images\soccer_ball.png");
+
+        moveX = 5;
+        moveY = 5;
+    }
+
+    public void Move()
+    {
+        XPos += moveX;
+        YPos += moveY;
     }
 }
--- BallSimulation/CodeFile1.cs
+++ BallSimulation/CodeFile1.cs
@@ -4,6 +4,11 @@ using System.Windows.Forms;
 
 class Stage : Form
 {
+    //ボールオブジェクト表示用
+    private PictureBox pictureBoxBall = new PictureBox();
+    private Ball ball;
+    private Timer timer = new Timer();
+
     public static void Main()
     {
         Application.Run(new Stage());
@@ -12,9 +17,34 @@ class Stage : Form
     public Stage()
     {
         //フォームの設定
-        Width = 800;
-        Height = 600;
+        Width = 1200;
+        Height = 800;
         Text = "ボールシミュレーション";
 
+        ball = new Ball();   //ボールオブジェクトの生成
+        PictureBox pictureBox = new PictureBox();
+
+        pictureBoxBall.Width = 100;
+        pictureBoxBall.Height = 100;
+        pictureBoxBall.Top = (int)ball.XPos;
+        pictureBoxBall.Left = (int)ball.YPos;
+        pictureBoxBall.Image = ball.Img;
+        pictureBoxBall.SizeMode = PictureBoxSizeMode.StretchImage;
+        pictureBoxBall.Parent = this;
+
+        timer.Interval = 10;        //ms
+        timer.Tick += Timer_Tick;   //時間経過で呼ぶメソッドを登録
+        timer.Start();              //タイマースタート
+
+
+    }
+
+    //指定した時間が経過すると呼ばれるメソッド
+    private void Timer_Tick(object sender, System.EventArgs e)
+    {
+        ball.Move();
+
+        pictureBoxBall.Top = (int)ball.XPos;
+        pictureBoxBall.Left = (int)ball.YPos;
     }
 }
