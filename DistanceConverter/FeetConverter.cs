6ef3843cfb4ebc66121565e529f9fffce514da78
AronKinoue <30123@ojs.ac.jp>
2020-06-22 13:51:33 +0900

    hennkou

--- DistanceConverter/FeetConverter.cs
+++ DistanceConverter/FeetConverter.cs
@@ -9,16 +9,19 @@ namespace DistanceConverter
     //フィートとメートルの単位変換クラス
     public static class FeetConverter
     {
+        //定数
+        private const double ratio = 0.3048;
+
         //メートルからフィートを求める（静的メソッドとして定義）
         public static double FromMeter(double meter)
         {
-            return meter * 0.3048;
+            return meter * ratio;
         }
 
         //フィートからメートルを求める（静的メソッドとして定義）
         public static double ToMeter(double feet)
         {
-            return feet * 3.28084;
+            return feet * 3.28084; //即値←数字を直接打つのはダメ×
         }
     }
 }
