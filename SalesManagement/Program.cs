7721008f81b14d6947753feae91ddf77921cf153
AronKinoue <30123@ojs.ac.jp>
2020-06-22 15:07:44 +0900

    listの追加

--- SalesManagement/Program.cs
+++ SalesManagement/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,34 @@ namespace SalesManagement
     {
         static void Main(string[] args)
         {
+
+        }
+
+        //売上データを読み込み、Saleオブジェクトのリストを返す
+        static List<Sale> ReadSals(string filePath)
+        {
+            //売上データを入れるリストオブジェクトを生成
+            var sales = new List<Sale>();
+
+            //ファイルから読み込み
+            string[] lines = File.ReadAllLines(filePath);
+
+            //読み込んだ行の数だけ繰り返す
+            foreach (string line in lines)
+            {
+                string[] items = line.Split(',');
+
+                Sale sale = new Sale
+                {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+
+                //saleオブジェクトをリストに追加
+                sales.Add(sale);
+            }
+            return sales;
         }
     }
 }
