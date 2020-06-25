0fc7a126a0903d4055224cdec86d110f19395182
AronKinoue <30123@ojs.ac.jp>
2020-06-25 14:59:20 +0900

    フォームデザイン完了

---
+++ AddressManagement/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace AddressManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgvPerson = new System.Windows.Forms.DataGridView();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbCompany = new System.Windows.Forms.TextBox();
+            this.tbMemo = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbDepartment = new System.Windows.Forms.TextBox();
+            this.tbPosition = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btImageOpen = new System.Windows.Forms.Button();
+            this.btImageClear = new System.Windows.Forms.Button();
+            this.cbGroup = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tbTelephone = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPerson)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "名前";
+            // 
+            // tbName
+            // 
+            this.tbName.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbName.Location = new System.Drawing.Point(12, 36);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(272, 31);
+            this.tbName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "住所";
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbAddress.Location = new System.Drawing.Point(12, 97);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(272, 31);
+            this.tbAddress.TabIndex = 5;
+            // 
+            // pbImage
+            // 
+            this.pbImage.Location = new System.Drawing.Point(645, 36);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(257, 217);
+            this.pbImage.TabIndex = 6;
+            this.pbImage.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(641, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "画像";
+            // 
+            // dgvPerson
+            // 
+            this.dgvPerson.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPerson.Location = new System.Drawing.Point(12, 356);
+            this.dgvPerson.Name = "dgvPerson";
+            this.dgvPerson.RowTemplate.Height = 21;
+            this.dgvPerson.Size = new System.Drawing.Size(890, 247);
+            this.dgvPerson.TabIndex = 10;
+            // 
+            // btAdd
+            // 
+            this.btAdd.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btAdd.Location = new System.Drawing.Point(908, 355);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(113, 248);
+            this.btAdd.TabIndex = 11;
+            this.btAdd.Text = "追加";
+            this.btAdd.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(340, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 24);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "会社名";
+            // 
+            // tbCompany
+            // 
+            this.tbCompany.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbCompany.Location = new System.Drawing.Point(344, 36);
+            this.tbCompany.Name = "tbCompany";
+            this.tbCompany.Size = new System.Drawing.Size(272, 31);
+            this.tbCompany.TabIndex = 13;
+            // 
+            // tbMemo
+            // 
+            this.tbMemo.Location = new System.Drawing.Point(12, 283);
+            this.tbMemo.Multiline = true;
+            this.tbMemo.Name = "tbMemo";
+            this.tbMemo.Size = new System.Drawing.Size(1009, 67);
+            this.tbMemo.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label7.Location = new System.Drawing.Point(340, 70);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 24);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "部署";
+            // 
+            // tbDepartment
+            // 
+            this.tbDepartment.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbDepartment.Location = new System.Drawing.Point(344, 97);
+            this.tbDepartment.Name = "tbDepartment";
+            this.tbDepartment.Size = new System.Drawing.Size(272, 31);
+            this.tbDepartment.TabIndex = 16;
+            // 
+            // tbPosition
+            // 
+            this.tbPosition.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbPosition.Location = new System.Drawing.Point(344, 158);
+            this.tbPosition.Name = "tbPosition";
+            this.tbPosition.Size = new System.Drawing.Size(272, 31);
+            this.tbPosition.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label8.Location = new System.Drawing.Point(340, 131);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 24);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "役職";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label9.Location = new System.Drawing.Point(12, 256);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 24);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "メモ";
+            // 
+            // btImageOpen
+            // 
+            this.btImageOpen.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btImageOpen.Location = new System.Drawing.Point(908, 36);
+            this.btImageOpen.Name = "btImageOpen";
+            this.btImageOpen.Size = new System.Drawing.Size(113, 104);
+            this.btImageOpen.TabIndex = 20;
+            this.btImageOpen.Text = "開く...";
+            this.btImageOpen.UseVisualStyleBackColor = true;
+            // 
+            // btImageClear
+            // 
+            this.btImageClear.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btImageClear.Location = new System.Drawing.Point(908, 146);
+            this.btImageClear.Name = "btImageClear";
+            this.btImageClear.Size = new System.Drawing.Size(113, 107);
+            this.btImageClear.TabIndex = 21;
+            this.btImageClear.Text = "クリア";
+            this.btImageClear.UseVisualStyleBackColor = true;
+            // 
+            // cbGroup
+            // 
+            this.cbGroup.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cbGroup.FormattingEnabled = true;
+            this.cbGroup.Location = new System.Drawing.Point(12, 221);
+            this.cbGroup.Name = "cbGroup";
+            this.cbGroup.Size = new System.Drawing.Size(268, 32);
+            this.cbGroup.TabIndex = 23;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(12, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 24);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "グループ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label10.Location = new System.Drawing.Point(12, 131);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(106, 24);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "電話番号";
+            // 
+            // tbTelephone
+            // 
+            this.tbTelephone.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbTelephone.Location = new System.Drawing.Point(12, 158);
+            this.tbTelephone.Name = "tbTelephone";
+            this.tbTelephone.Size = new System.Drawing.Size(272, 31);
+            this.tbTelephone.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1030, 616);
+            this.Controls.Add(this.tbTelephone);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.cbGroup);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btImageClear);
+            this.Controls.Add(this.btImageOpen);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tbPosition);
+            this.Controls.Add(this.tbDepartment);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tbMemo);
+            this.Controls.Add(this.tbCompany);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btAdd);
+            this.Controls.Add(this.dgvPerson);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.tbAddress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPerson)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgvPerson;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbCompany;
+        private System.Windows.Forms.TextBox tbMemo;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbDepartment;
+        private System.Windows.Forms.TextBox tbPosition;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btImageOpen;
+        private System.Windows.Forms.Button btImageClear;
+        private System.Windows.Forms.ComboBox cbGroup;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox tbTelephone;
+    }
+}
+
