f4df7fc39850500fcfe23147590ca9af09400796
AronKinoue <30123@ojs.ac.jp>
2020-06-25 15:25:30 +0900

    carクラスの作成

---
+++ ListCollectionSample/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListCollectionSample
+{
+    class Car
+    {
+        //車名
+        public string Name { get; set; }
+
+        //メーカー
+        public string Maker { get; set; }
+
+        //カテゴリ
+        public string Category { get; set; }
+    }
+}
