edde1285a05ad9e98df3c714bc43156d113e8f65
AronKinoue <30123@ojs.ac.jp>
2020-07-06 10:03:01 +0900

    fairukannsei

---
+++ ListCollectionSample_tuduki/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace ListCollectionSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbCategory = new System.Windows.Forms.TextBox();
+            this.dgvCarData = new System.Windows.Forms.DataGridView();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.btOpenImage = new System.Windows.Forms.Button();
+            this.ofdOpenImage = new System.Windows.Forms.OpenFileDialog();
+            this.btClearImage = new System.Windows.Forms.Button();
+            this.btModify = new System.Windows.Forms.Button();
+            this.btDelete = new System.Windows.Forms.Button();
+            this.cbMaker = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.新規入力ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.終了XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btSave = new System.Windows.Forms.Button();
+            this.btRead = new System.Windows.Forms.Button();
+            this.sfdSaveData = new System.Windows.Forms.SaveFileDialog();
+            this.ofdOpenData = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "車名";
+            // 
+            // tbName
+            // 
+            this.tbName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbName.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbName.Location = new System.Drawing.Point(120, 32);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(346, 31);
+            this.tbName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(12, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "メーカー";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(12, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 24);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "カテゴリ";
+            // 
+            // tbCategory
+            // 
+            this.tbCategory.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbCategory.Location = new System.Drawing.Point(120, 155);
+            this.tbCategory.Name = "tbCategory";
+            this.tbCategory.Size = new System.Drawing.Size(315, 31);
+            this.tbCategory.TabIndex = 1;
+            // 
+            // dgvCarData
+            // 
+            this.dgvCarData.AllowUserToAddRows = false;
+            this.dgvCarData.AllowUserToDeleteRows = false;
+            this.dgvCarData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvCarData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCarData.Location = new System.Drawing.Point(15, 209);
+            this.dgvCarData.MultiSelect = false;
+            this.dgvCarData.Name = "dgvCarData";
+            this.dgvCarData.ReadOnly = true;
+            this.dgvCarData.RowTemplate.Height = 21;
+            this.dgvCarData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCarData.Size = new System.Drawing.Size(652, 373);
+            this.dgvCarData.TabIndex = 2;
+            this.dgvCarData.Click += new System.EventHandler(this.dgvCarData_Click);
+            // 
+            // btAdd
+            // 
+            this.btAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btAdd.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btAdd.Location = new System.Drawing.Point(688, 209);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(100, 45);
+            this.btAdd.TabIndex = 3;
+            this.btAdd.Text = "登録";
+            this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
+            // 
+            // pbImage
+            // 
+            this.pbImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImage.Location = new System.Drawing.Point(491, 32);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(176, 154);
+            this.pbImage.TabIndex = 4;
+            this.pbImage.TabStop = false;
+            // 
+            // btOpenImage
+            // 
+            this.btOpenImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btOpenImage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btOpenImage.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btOpenImage.Location = new System.Drawing.Point(688, 32);
+            this.btOpenImage.Name = "btOpenImage";
+            this.btOpenImage.Size = new System.Drawing.Size(100, 43);
+            this.btOpenImage.TabIndex = 5;
+            this.btOpenImage.Text = "開く...";
+            this.btOpenImage.UseVisualStyleBackColor = true;
+            this.btOpenImage.Click += new System.EventHandler(this.btOpenImage_Click);
+            // 
+            // ofdOpenImage
+            // 
+            this.ofdOpenImage.FileName = "openFileDialog1";
+            // 
+            // btClearImage
+            // 
+            this.btClearImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btClearImage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btClearImage.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btClearImage.Location = new System.Drawing.Point(688, 82);
+            this.btClearImage.Name = "btClearImage";
+            this.btClearImage.Size = new System.Drawing.Size(100, 38);
+            this.btClearImage.TabIndex = 6;
+            this.btClearImage.Text = "クリア";
+            this.btClearImage.UseVisualStyleBackColor = true;
+            this.btClearImage.Click += new System.EventHandler(this.btClearImage_Click);
+            // 
+            // btModify
+            // 
+            this.btModify.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btModify.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btModify.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btModify.Location = new System.Drawing.Point(688, 272);
+            this.btModify.Name = "btModify";
+            this.btModify.Size = new System.Drawing.Size(100, 45);
+            this.btModify.TabIndex = 7;
+            this.btModify.Text = "変更";
+            this.btModify.UseVisualStyleBackColor = true;
+            this.btModify.Click += new System.EventHandler(this.btModify_Click);
+            // 
+            // btDelete
+            // 
+            this.btDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btDelete.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btDelete.Location = new System.Drawing.Point(688, 335);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(100, 45);
+            this.btDelete.TabIndex = 3;
+            this.btDelete.Text = "削除";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // cbMaker
+            // 
+            this.cbMaker.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbMaker.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cbMaker.FormattingEnabled = true;
+            this.cbMaker.Location = new System.Drawing.Point(120, 93);
+            this.cbMaker.Name = "cbMaker";
+            this.cbMaker.Size = new System.Drawing.Size(250, 32);
+            this.cbMaker.TabIndex = 8;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルFToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルFToolStripMenuItem
+            // 
+            this.ファイルFToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.新規入力ToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.終了XToolStripMenuItem});
+            this.ファイルFToolStripMenuItem.Name = "ファイルFToolStripMenuItem";
+            this.ファイルFToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.ファイルFToolStripMenuItem.Text = "ファイル(&F)";
+            // 
+            // 新規入力ToolStripMenuItem
+            // 
+            this.新規入力ToolStripMenuItem.Name = "新規入力ToolStripMenuItem";
+            this.新規入力ToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.新規入力ToolStripMenuItem.Text = "新規入力";
+            this.新規入力ToolStripMenuItem.Click += new System.EventHandler(this.新規入力ToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(152, 6);
+            // 
+            // 終了XToolStripMenuItem
+            // 
+            this.終了XToolStripMenuItem.Name = "終了XToolStripMenuItem";
+            this.終了XToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.終了XToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.終了XToolStripMenuItem.Text = "終了(&X)";
+            this.終了XToolStripMenuItem.Click += new System.EventHandler(this.終了XToolStripMenuItem_Click);
+            // 
+            // btSave
+            // 
+            this.btSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btSave.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btSave.Location = new System.Drawing.Point(688, 458);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(100, 45);
+            this.btSave.TabIndex = 10;
+            this.btSave.Text = "保存";
+            this.btSave.UseVisualStyleBackColor = false;
+            this.btSave.Click += new System.EventHandler(this.btSave_Click);
+            // 
+            // btRead
+            // 
+            this.btRead.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btRead.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btRead.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btRead.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btRead.Location = new System.Drawing.Point(688, 521);
+            this.btRead.Name = "btRead";
+            this.btRead.Size = new System.Drawing.Size(100, 45);
+            this.btRead.TabIndex = 10;
+            this.btRead.Text = "読込";
+            this.btRead.UseVisualStyleBackColor = false;
+            this.btRead.Click += new System.EventHandler(this.btRead_Click);
+            // 
+            // ofdOpenData
+            // 
+            this.ofdOpenData.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.btRead);
+            this.Controls.Add(this.btSave);
+            this.Controls.Add(this.cbMaker);
+            this.Controls.Add(this.btModify);
+            this.Controls.Add(this.btClearImage);
+            this.Controls.Add(this.btOpenImage);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.btAdd);
+            this.Controls.Add(this.dgvCarData);
+            this.Controls.Add(this.tbCategory);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "車登録アプリ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbCategory;
+        private System.Windows.Forms.DataGridView dgvCarData;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Button btOpenImage;
+        private System.Windows.Forms.OpenFileDialog ofdOpenImage;
+        private System.Windows.Forms.Button btClearImage;
+        private System.Windows.Forms.Button btModify;
+        private System.Windows.Forms.Button btDelete;
+        private System.Windows.Forms.ComboBox cbMaker;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 新規入力ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem 終了XToolStripMenuItem;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.Button btRead;
+        private System.Windows.Forms.SaveFileDialog sfdSaveData;
+        private System.Windows.Forms.OpenFileDialog ofdOpenData;
+    }
+}
+
---
+++ ListCollectionSample_tuduki/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListCollectionSample
+{
+    public partial class Form1 : Form
+    {
+        //車データを入れるバインディングリスト
+        BindingList<Car> _Cars = new BindingList<Car>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            dgvCarData.DataSource = _Cars;
+
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+            if (tbName.Text == "")
+            {
+                MessageBox.Show("車名を入力してください");
+                return;
+            }
+            //Carオブジェクトの作成
+            Car obj = new Car
+            {
+                Name = tbName.Text,
+                Maker = cbMaker.Text,
+                Category = tbCategory.Text,
+                carPic = pbImage.Image
+            };
+            //メーカーをコンボボックスの入力候補に登録
+            setComboBoxMaker(cbMaker.Text);
+
+            //BindingListへ登録
+            //_Cars.Add(obj);
+            _Cars.Insert(0, obj); //リストの先頭（インデックス０）へ追加
+
+            //高さを自動調整
+            dgvCarData.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+            //次の入力に備えて各項目をクリア
+            inputItemAllClear();
+            initButtons();
+            dgvCarData.ClearSelection();    //選択行をクリア
+            //dgvCarData_Click(sender, e);  //イベントハンドラを呼び出す
+
+        }
+
+        //入力項目を全クリア
+        private void inputItemAllClear()
+        {
+            tbName.Text = "";
+            cbMaker.Text = "";
+            tbCategory.Text = "";
+            pbImage.Image = null;
+        }
+
+        //メーカーコンボボックスの入力候補登録
+        private void setComboBoxMaker(string maker)
+        {
+            if (!cbMaker.Items.Contains(maker))
+            {
+                //コンボボックスの候補に追加
+                cbMaker.Items.Add(maker);
+            }
+        }
+
+        private void btOpenImage_Click(object sender, EventArgs e)
+        {
+            if (ofdOpenImage.ShowDialog() == DialogResult.OK)
+            {
+                //選択した画像をピクチャーボックスに表示
+                pbImage.Image = Image.FromFile(ofdOpenImage.FileName);
+                //ピクチャーボックスのサイズに画像を調整
+                pbImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void btClearImage_Click(object sender, EventArgs e)
+        {
+            pbImage.Image = null;
+        }
+
+        private void dgvCarData_Click(object sender, EventArgs e)
+        {
+            if (dgvCarData.CurrentRow == null)
+                return;
+
+            //選択したレコードを取り出す
+            //データグリッドビューで選択した行のインデックスを元に
+            //BindingListのデータを取得する
+            Car selectedCar = _Cars[dgvCarData.CurrentRow.Index];
+            tbName.Text = selectedCar.Name;
+            cbMaker.Text = selectedCar.Maker;
+            tbCategory.Text = selectedCar.Category;
+            pbImage.Image = selectedCar.carPic;
+
+        }
+
+        private void btModify_Click(object sender, EventArgs e)
+        {
+            //変更対象のレコード（オブジェクト）
+            Car selectedCar = _Cars[dgvCarData.CurrentRow.Index];
+            selectedCar.Name = tbName.Text;
+            selectedCar.Maker = cbMaker.Text;
+            selectedCar.Category = tbCategory.Text;
+            selectedCar.carPic = pbImage.Image;
+
+            dgvCarData.Refresh();       //データグリッドビューの再描画
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initButtons();
+        }
+
+        private void initButtons()
+        {
+            if (_Cars.Count > 0)
+            {
+                btModify.Enabled = true;
+                btDelete.Enabled = true;
+            }
+            else
+            {
+                btModify.Enabled = false;
+                btDelete.Enabled = false;
+            }
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            _Cars.RemoveAt(dgvCarData.CurrentRow.Index);
+            initButtons();
+            inputItemAllClear();
+            dgvCarData.ClearSelection();
+        }
+
+        private void 終了XToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void 新規入力ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            inputItemAllClear();
+        }
+
+        private void btSave_Click(object sender, EventArgs e)
+        {
+            if (sfdSaveData.ShowDialog() == DialogResult.OK)
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+
+                using (FileStream fs = new FileStream(sfdSaveData.FileName, FileMode.Create))
+                {
+                    try
+                    {
+                        //シリアル化して保存
+                        formatter.Serialize(fs, _Cars);
+                    }
+                    catch (SerializationException se)
+                    {
+                        Console.WriteLine("Failed to serialize. Reason: " + se.Message);
+                        throw;
+                    }
+                }
+            }
+        }
+
+        private void btRead_Click(object sender, EventArgs e)
+        {
+            //オープンファイルダイアログを表示
+            if (ofdOpenData.ShowDialog() == DialogResult.OK)
+            {
+                using (FileStream fs = new FileStream(ofdOpenData.FileName, FileMode.Open))
+                {
+                    try
+                    {
+                        BinaryFormatter formatter = new BinaryFormatter();
+                        //逆シリアル化して読み込む
+                        _Cars = (BindingList<Car>)formatter.Deserialize(fs);
+                        //データグリッドビューに再設定
+                        dgvCarData.DataSource = _Cars;
+                        //選択されている箇所を各コントロールへ表示
+                        dgvCarData_Click(sender, e);
+                    }
+                    catch (SerializationException se)
+                    {
+                        Console.WriteLine("Failed to deserialize. Reason: " + se.Message);
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
